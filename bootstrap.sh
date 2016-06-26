f79875164e3fc13a323aeb111f97cb8c19aec9e1
fen <f3nb0x@gmail.com>
2016-06-26 18:18:44 +0800

    配置初始化

---
+++ bootstrap.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Add the code to Vagrantfile
+# config.vm.provision :shell, path: "bootstrap.sh"
+
+# change source mirror
+cd /etc/apt/
+mv sources.list sources.list.bak
+bash -c "cat /vagrant/sources.list.aliyun ./sources.list.bak > sources.list"
+apt-get update -y
+apt-get upgrade -y
+
+# instal build
+apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev
+
+# install nginx & git
+apt-get install -y nginx git
+
+# install pyenv
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+echo 'export PATH="/home/vagrant/.pyenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+
+# install pyrhon
+pyenv install 3.5.1
